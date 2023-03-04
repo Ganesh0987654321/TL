11fa1dd379711d7be0ab4c8f133bc5583c8c2fa0
Ganesh0987654321 <ganeshshirsath60@gmail.com>
2023-03-04 16:26:27 +0530

    First push from the Intellij

---
+++ src/main/java/com/example/TL/TlApplication.java
@@ -0,0 +1,14 @@
+package com.example.TL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TlApplication.class, args);
+		System.out.println("Welcome Ganesh");
+	}
+
+}
